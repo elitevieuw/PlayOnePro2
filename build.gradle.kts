c333745fc506cb3574fa41a7d03b502c4a5d146c
elitevieuw <cemcaliskan1000@gmail.com>
2025-11-11 04:59:27 +0000

    Critical fix: Update root build.gradle.kts to use Kotlin 2.0.26 via version catalog - resolves version conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "8.7.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.9.25" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
 }
