49fd2c304a768f7b2402daa40c1c97fa0d5de0bb
elitevieuw <cemcaliskan1000@gmail.com>
2025-11-11 10:33:51 +0100

    Add Channel model for PlayOnePro2 data/model layer

---
+++ app/src/main/java/com/playonepro/app/data/model/Channel.kt
@@ -0,0 +1,9 @@
+package com.playonepro.app.data.model
+
+data class Channel(
+    val id: String,
+    val name: String,
+    val logoUrl: String,
+    val streamUrl: String,
+    val category: String
+)
